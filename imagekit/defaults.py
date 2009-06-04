81f4b950cdea6302ca1cdad7d5c001ea4a64d81f
Justin Driscoll <justin.driscoll@gmail.com>
2009-06-04 11:06:40 -0400

    Restored defaults

--- imagekit/defaults.py
+++ imagekit/defaults.py
@@ -4,8 +4,8 @@ from imagekit.specs import ImageSpec
 from imagekit import processors
     
 class ResizeThumbnail(processors.Resize):
-    width = 200
-    height = 100
+    width = 100
+    height = 50
     crop = True
     
 class EnhanceSmall(processors.Adjustment):
