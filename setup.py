ede345a780ca5559464a57864795c9a89ff6f613
Bryan Veloso <bryan@revyver.com>
2011-02-10 14:24:11 -0800

    Massaging setup.py.

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 #/usr/bin/env python
-from setuptools import setup, find_packages
- 
+from distutils.core import setup
+
 setup(
     name='django-imagekit',
     version='0.3.3',
     description='Automated image processing for Django models.',
     author='Justin Driscoll',
     author_email='justin@driscolldev.com',
-    url='http://bitbucket.org/jdriscoll/django-imagekit/',
-    packages=find_packages(),
+    url='http://github.com/jdriscoll/django-imagekit/',
+    packages=[
+        'imagekit'
+    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
@@ -17,12 +19,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Django',
-    ],
-    # Make setuptools include all data files under version control,
-    # svn and CVS by default
-    include_package_data=True,
-    zip_safe=False,
-    # Tells setuptools to download setuptools_git before running setup.py so
-    # it can find the data files under Hg version control.
-    setup_requires=['setuptools_hg'],
+        'Topic :: Utilities'
+    ]
 )
+
+
