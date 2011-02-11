5d1c8982837e10b94d41bc0d93050f7d6024f767
Bryan Veloso <bryan@revyver.com>
2011-02-10 19:00:26 -0800

    Flush newer records first. Also, spacing.

--- imagekit/management/commands/ikflush.py
+++ imagekit/management/commands/ikflush.py
@@ -16,16 +16,16 @@ class Command(BaseCommand):
 
 def flush_cache(apps, options):
     """ Clears the image cache
-    
+
     """
     apps = [a.strip(',') for a in apps]
     if apps:
         for app_label in apps:
-            app = cache.get_app(app_label)    
+            app = cache.get_app(app_label)
             models = [m for m in cache.get_models(app) if issubclass(m, ImageModel)]
             for model in models:
                 print 'Flushing cache for "%s.%s"' % (app_label, model.__name__)
-                for obj in model.objects.all():
+                for obj in model.objects.order_by('-id'):
                     for spec in model._ik.specs:
                         prop = getattr(obj, spec.name(), None)
                         if prop is not None:
