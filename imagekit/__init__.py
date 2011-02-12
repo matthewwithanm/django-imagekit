1b29437180834ec5196b5b07c6ee24fff0bf996d
Bryan Veloso <bryan@revyver.com>
2011-02-12 14:04:42 -0800

    Bumping the version number.

--- imagekit/__init__.py
+++ imagekit/__init__.py
@@ -2,7 +2,7 @@
 Django ImageKit
 
 Author: Justin Driscoll <justin.driscoll@gmail.com>
-Version: 0.3.4
+Version: 0.3.5
 
 """
 __author__ = 'Justin Driscoll, Bryan Veloso, Greg Newman, Chris Drackett'
