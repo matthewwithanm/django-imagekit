0763afd0699e56ae6ac4d7b58b3b8328e9fe8e07
Nitya Oberoi <nitya@yipit.com>
2011-03-21 16:46:41 -0400

    Fix in processors to pass tests

--- imagekit/processors.py
+++ imagekit/processors.py
@@ -100,42 +100,43 @@ class Resize(ImageProcessor):
     @classmethod
     def process(cls, img, fmt, obj):
         cur_width, cur_height = img.size
+        default_width = float(cls.width or cls.max_width or cur_width)
+        default_height = float(cls.height or cls.max_height or cur_height)
         if cls.crop:
-            crop_horz = getattr(obj, obj._ik.crop_horz_field, 1)
-            crop_vert = getattr(obj, obj._ik.crop_vert_field, 1)
-            ratio = max(float(cls.width)/cur_width, float(cls.height)/cur_height)
-            resize_x, resize_y = ((cur_width * ratio), (cur_height * ratio))
-            crop_x, crop_y = (abs(cls.width - resize_x), abs(cls.height - resize_y))
+            ratio = max(default_width/cur_width, default_height/cur_height)
+        else:
+            ratio = min(default_width/cur_width, default_height/cur_height)
+        resize_x, resize_y = (cur_width * ratio, cur_height * ratio)
+        if cls.max_width: 
+            resize_x = min(resize_x, float(cls.max_width))
+            resize_y = (resize_x/cur_width) * cur_height
+        if cls.max_height: 
+            resize_y = min(resize_y, float(cls.max_height))
+            resize_x = (resize_y/cur_height) * cur_width
+
+        if cls.crop:
+            crop_horz = cls.crop_horz_field or getattr(obj, obj._ik.crop_horz_field, 1)
+            crop_vert = cls.crop_vert_field or getattr(obj, obj._ik.crop_vert_field, 1)
+            crop_x, crop_y = (abs(default_width - resize_x), abs(default_height - resize_y))
             x_diff, y_diff = (int(crop_x / 2), int(crop_y / 2))
             box_left, box_right = {
-                0: (0, cls.width),
-                1: (int(x_diff), int(x_diff + cls.width)),
+                0: (0, default_width),
+                1: (int(x_diff), int(x_diff + default_width)),
                 2: (int(crop_x), int(resize_x)),
             }[crop_horz]
             box_upper, box_lower = {
-                0: (0, cls.height),
-                1: (int(y_diff), int(y_diff + cls.height)),
+                0: (0, default_height),
+                1: (int(y_diff), int(y_diff + default_height)),
                 2: (int(crop_y), int(resize_y)),
             }[crop_vert]
             box = (box_left, box_upper, box_right, box_lower)
             img = img.resize((int(resize_x), int(resize_y)), Image.ANTIALIAS).crop(box)
         else:
-            if not (cls.max_height or cls.max_width):
-                if not cls.width is None and not cls.height is None:
-                    ratio = min(float(cls.width)/cur_width,
-                                float(cls.height)/cur_height)
-                else:
-                    if cls.width is None:
-                        ratio = float(cls.height)/cur_height
-                    else:
-                        ratio = float(cls.width)/cur_width
-            else:
-                ratio = max(float(cls.max_width or 0)/cur_width, float(cls.max_height or 0)/cur_height)
-            new_dimensions = (int(round(cur_width*ratio)), int(round(cur_height*ratio)))
-            if new_dimensions[0] > cur_width or new_dimensions[1] > cur_height:
+            if resize_x > cur_width or \
+               resize_y > cur_height:
                 if not cls.upscale:
                     return img, fmt
-            img = img.resize(new_dimensions, Image.ANTIALIAS)
+            img = img.resize((int(resize_x), int(resize_y)), Image.ANTIALIAS)
         return img, fmt
 
 class Transpose(ImageProcessor):
