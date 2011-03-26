f777c08b1725317b07c7e2b6b539e54d0b96288f
Bryan Veloso <bryan@revyver.com>
2011-03-26 00:46:17 -0700

    Whitespacing.

--- imagekit/models.py
+++ imagekit/models.py
@@ -149,6 +149,8 @@ class ImageModel(models.Model):
     def clear_cache(self, **kwargs):
         assert self._get_pk_val() is not None, "%s object can't be deleted because its %s attribute is set to None." % (self._meta.object_name, self._meta.pk.attname)
         self._clear_cache()
+
+
 post_delete.connect(ImageModel.clear_cache, sender=ImageModel)
 
 
