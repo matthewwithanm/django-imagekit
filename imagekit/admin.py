ab7442034c679aaf8e32bb08813a2204e85a9f6e
fish2000 <fish2000@gmail.com>
2011-06-09 09:16:13 -0400

    ICCModel admin improvements: redesigned {r,g,b}TRC graphs; refined JSON calls which use the best available among the ujson/yajl/simplejson modules before falling back to the standard lib; skeleton for admin CSS; cleaner code; and more.

--- imagekit/admin.py
+++ imagekit/admin.py
@@ -1,8 +1,25 @@
-import os, json
+import os
 from django.conf import settings
 from django.contrib import admin
 from imagekit.models import ICCModel
+from imagekit.ICCProfile import ADict, AODict
 from imagekit.etc.cieXYZ import cieYxy3
+from jogging import logging as logg
+
+try:
+    import ujson as json
+except ImportError:
+    logg.info("--- Loading yajl in leu of ujson")
+    try:
+        import yajl as json
+    except ImportError:
+        logg.info("--- Loading simplejson in leu of yajl")
+        try:
+            import simplejson as json
+        except ImportError:
+            logg.info("--- Loading stdlib json module in leu of simplejson")
+            import json
+
 
 # The icon immediately below is copyright (C) 2011 Yusuke Kamiyamane -- All of his rights are reserved.
 # It's licensed under Creative Commons Attribution 3.0: http://creativecommons.org/licenses/by/3.0/
@@ -11,11 +28,14 @@ icon = u"""
 <img width="16" height="16" title="Download ICC File" alt="Download ICC File" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAgdJREFUeNqEk79r20AUx786R6dKxUUNNWmyeuxQCsZkaYd0DvkHOiRbpgydDfbgpUO0GUqzpYXMxf0HMrRTPHpKPSamoDrBgtrWyVLeO/2wAgUfHLovp/e993n3zmi320iSBPkwDGOfPttYP8adTqe/kQUhiqJ8Y6fb7X5eF91qtY75WxhUKhXM53Mt4zjGdf8QVcfUP2/tfcHvox+omo7WL8/eQyllFAY8hBCQUiIIAmO5XMKWAjXX0nucnS0kaparNe8vFovCYJ9qoJk5C8uy3g4GA9yPHdxMpQ5I1ACxNcEL9Sw9rGxA7v9n3t19JC+VQvj6FbIgxk0NmIWZ+9eHcKop894WMx8Rs5kxn+Hi4h7n5/+09rynKwNOhZmkLeDWysxUA8sqmOOY8WqZDjCbzR4bqIhSpDS1QRIhorXKNDMrFVK/qMKQDIQ24FT4xHebPSBMeSkUm71eLjXzwYGgskyzZtuA7/srA3Y8ufwD06nqHz69kfj58S8cM9VNYvY87/twOPyVF5Xq5msDZmEDIW1YbsqolA8pbLglZsdxriaTybfSxdxpPGbRNVARVKj0ZCTWXBOevE+9EjQajVuKyedMZ8As3MqnTW7TaWZuonlqFrrMXK/XMRqNVo/Pdd0P1MY76x4PMY8pk6+8DsOQO1G/Yr7LJzSfs9kaj7s87XywwYMAAwASbxzfXa6dmwAAAABJRU5ErkJggg==" />
 """
 
-xy = lambda ill: (ill['X'] / (ill['X']+ill['Y']+ill['Z']), ill['Y'] / (ill['X']+ill['Y']+ill['Z']))
+xy = lambda n: (n.X / (n.X + n.Y + n.Z), n.Y / (n.X + n.Y + n.Z))
 
 class ICCModelAdmin(admin.ModelAdmin):
     
     class Media:
+        css = { 'all': (
+            os.path.join(settings.STATIC_URL, 'css/iccprofile-admin.css'),
+        )}
         js = (
             'https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js',
             os.path.join(settings.STATIC_URL, 'flot/jquery.flot.js'),
@@ -125,7 +145,7 @@ class ICCModelAdmin(admin.ModelAdmin):
                 },
                 'xaxis': dict(show=True, ticks=4, min=-0.1, max=0.85),
                 'yaxis': dict(show=True, ticks=4, min=-0.1, max=0.85),
-                'grid': dict(show=True),
+                'grid': dict(show=False),
             }
 
             # gray line
@@ -138,13 +158,13 @@ class ICCModelAdmin(admin.ModelAdmin):
             
             # tristimulii
             if 'rXYZ' in icc.tags.keys():
-                series.append({ 'color': "#FC1919", 'data': [xy(icc.tags.get('rXYZ'))], 'clickable': True, 'hoverable': True })
+                series.append({ 'color': "#FC1919", 'data': [xy(icc.tags.rXYZ)], 'clickable': True, 'hoverable': True })
             if 'gXYZ' in icc.tags.keys():
-                series.append({ 'color': "#19FA19", 'data': [xy(icc.tags.get('gXYZ'))], 'clickable': True, 'hoverable': True })
+                series.append({ 'color': "#19FA19", 'data': [xy(icc.tags.gXYZ)], 'clickable': True, 'hoverable': True })
             if 'bXYZ' in icc.tags.keys():
-                series.append({ 'color': "#1991FF", 'data': [xy(icc.tags.get('bXYZ'))], 'clickable': True, 'hoverable': True })
+                series.append({ 'color': "#1991FF", 'data': [xy(icc.tags.bXYZ)], 'clickable': True, 'hoverable': True })
             if 'wtpt' in icc.tags.keys():
-                series.append({ 'color': "#000000", 'data': [xy(icc.tags.get('wtpt'))], 'clickable': True, 'hoverable': True })
+                series.append({ 'color': "#000000", 'data': [xy(icc.tags.wtpt)], 'clickable': True, 'hoverable': True })
             
             # CIE'31 horseshoe curve
             series.append({ 'color': '#EFEFEF', 'data': cieYxy3, 'points': dict(show=False, fill=False), })
@@ -179,8 +199,12 @@ class ICCModelAdmin(admin.ModelAdmin):
     def icc_flot_rgb_trc(self, obj):
         if obj.icc:
             icc = obj.icc
-            series = []
             transformer = icc.transformer
+            graphs = AODict()
+            graphs.update({ 'red':      ADict(series=[]), })
+            graphs.update({ 'green':    ADict(series=[]), })
+            graphs.update({ 'blue':     ADict(series=[]), })
+            graphs.update({ 'luma':     ADict(series=[]), })
             
             if not transformer:
                 return u'<i style="color: lightgray;">No Transformer</i>' 
@@ -188,45 +212,56 @@ class ICCModelAdmin(admin.ModelAdmin):
             # flot config options
             options = {
                 'series': {
-                    'points': dict(show=True, fill=True, radius=2),
-                    'lines': dict(show=True, fill=False, lineWidth=1),
+                    'points': dict(show=False, fill=True, radius=2),
+                    'lines': dict(show=True, fill=True, lineWidth=1),
                 },
                 'xaxis': dict(show=True, ticks=4, min=0, max=255),
                 'yaxis': dict(show=True, ticks=4, min=0.0, max=1.0),
-                'grid': dict(show=True),
+                'grid': dict(show=False),
             }
             
             trilin = transformer.getRGBTristimulusLinearizer()
             
             if 'rTRC' in icc.tags.keys():
-                series.append({ 'color': "#FC1919", 'data': [[i, trilin.fr(i).item()] for i in xrange(256)], 'clickable': True, 'hoverable': True })
+                graphs.red.series.append({ 'color': "#FC1919", 'data': [[i, trilin.fr(i).item()] for i in xrange(256)], 'clickable': True, 'hoverable': True })
             if 'gTRC' in icc.tags.keys():
-                series.append({ 'color': "#19FA19", 'data': [[i, trilin.fg(i).item()] for i in xrange(256)], 'clickable': True, 'hoverable': True })
+                graphs.green.series.append({ 'color': "#19FA19", 'data': [[i, trilin.fg(i).item()] for i in xrange(256)], 'clickable': True, 'hoverable': True })
             if 'bTRC' in icc.tags.keys():
-                series.append({ 'color': "#1991FF", 'data': [[i, trilin.fb(i).item()] for i in xrange(256)], 'clickable': True, 'hoverable': True })
+                graphs.blue.series.append({ 'color': "#1991FF", 'data': [[i, trilin.fb(i).item()] for i in xrange(256)], 'clickable': True, 'hoverable': True })
             
             
-            return u"""
-                <span class="icc-rgb-trc">
-                    <div class="rgb-trc" id="rgb-trc-%s" style="width: %spx; height: %spx;"></div>
-                </span>
-                <script type="text/javascript">
-                    $(document).ready(function () {
-                        $.plot(
-                            $('#rgb-trc-%s'),
-                            %s,
-                            %s
-                        );
-                    });
-                </script>
-            """ % (
-                obj.icchash,
-                250, 250,
-                obj.icchash,
-                json.dumps(series),
-                json.dumps(options),
-            )
-        
+            outopts = json.dumps(options)
+            out = u"""
+                <div class="icc-rgb-trc" style="width: 375px;">
+            """
+            
+            for channel, bitstream in graphs.items():
+                if len(bitstream.series):
+                    out += """
+                        <div class="rgb-trc" id="rgb-trc-%s-%s" style="width: %spx; height: %spx;"></div>
+                        <script type="text/javascript">
+                            $(document).ready(function () {
+                                $.plot(
+                                    $('#rgb-trc-%s-%s'),
+                                    %s,
+                                    %s
+                                );
+                            });
+                        </script>
+                    """ % (
+                        obj.icchash, channel,
+                        120, 120,
+                        obj.icchash, channel,
+                        json.dumps(bitstream.series),
+                        outopts,
+                    )
+            
+            out += u"""
+                </div>
+            """
+            
+            
+            return out
         # no-icc default
         return u'<i style="color: lightgray;">None</i>'
     icc_flot_rgb_trc.short_description = "Tone Response Curves"
@@ -236,16 +271,6 @@ class ICCModelAdmin(admin.ModelAdmin):
     '''
     ModelAdmin overrides
     '''
-    
-    """
-    def save_model(self, request, obj, form, change):
-        from django.db import IntegrityError
-        
-        try:
-            obj.save()
-        except IntegrityError:
-            self.message_user(request, "YO DOGG: that ICC file's signature was already in the database.")
-    """
     
     def save_form(self, request, form, change):
         maybe = super(ICCModelAdmin, self).save_form(request, form, change)
