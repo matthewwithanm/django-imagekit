6f997650969692c285157ff890593862caa147bc
fish2000 <fish2000@gmail.com>
2011-06-14 18:02:11 -0400

    Cleaned up neuquant.py and added a demo in __main__.

--- imagekit/neuquant.py
+++ imagekit/neuquant.py
@@ -8,6 +8,7 @@ Copyright (c) 2011 OST, LLC.
 """
 
 import os
+from jogging import logging as logg
 
 try:
     import PIL
@@ -107,11 +108,11 @@ class NeuQuant:
         self.NCYCLES = 100 # Number of learning cycles
         self.NETSIZE = colors # Number of colours used
         self.SPECIALS = 3 # Number of reserved colours used
-        self.BGCOLOR = self.SPECIALS-1 # Reserved background colour
+        self.BGCOLOR = self.SPECIALS - 1 # Reserved background colour
         self.CUTNETSIZE = self.NETSIZE - self.SPECIALS
         self.MAXNETPOS = self.NETSIZE - 1
         
-        self.INITRAD = self.NETSIZE/8 # For 256 colours, radius starts at 32
+        self.INITRAD = self.NETSIZE / 8 # For 256 colours, radius starts at 32
         self.RADIUSBIASSHIFT = 6
         self.RADIUSBIAS = 1 << self.RADIUSBIASSHIFT
         self.INITBIASRADIUS = self.INITRAD * self.RADIUSBIAS
@@ -121,7 +122,7 @@ class NeuQuant:
         self.INITALPHA = 1 << self.ALPHABIASSHIFT # biased by 10 bits
         
         self.GAMMA = 1024.0
-        self.BETA = 1.0/1024.0
+        self.BETA = 1.0 / 1024.0
         self.BETAGAMMA = self.BETA * self.GAMMA
         
         self.network = np.empty((self.NETSIZE, 3), dtype='float64') # The network itself
@@ -180,7 +181,7 @@ class NeuQuant:
         
         for i in range(self.SPECIALS, self.NETSIZE):
             p = self.network[i]
-            p[:] = (255.0 * (i-self.SPECIALS)) / self.CUTNETSIZE
+            p[:] = (255.0 * (i - self.SPECIALS)) / self.CUTNETSIZE
             
             self.freq[i] = 1.0 / self.NETSIZE
             self.bias[i] = 0.0
@@ -190,33 +191,33 @@ class NeuQuant:
     def altersingle(self, alpha, i, b, g, r):
         """Move neuron i towards biased (b,g,r) by factor alpha"""
         n = self.network[i] # Alter hit neuron
-        n[0] -= (alpha*(n[0] - b))
-        n[1] -= (alpha*(n[1] - g))
-        n[2] -= (alpha*(n[2] - r))
+        n[0] -= (alpha * (n[0] - b))
+        n[1] -= (alpha * (n[1] - g))
+        n[2] -= (alpha * (n[2] - r))
     
     def geta(self, alpha, rad):
         try:
             return self.a_s[(alpha, rad)]
         except KeyError:
-            length = rad*2-1
-            mid = length/2
-            q = np.array(range(mid-1,-1,-1)+range(-1,mid))
-            a = alpha*(rad*rad - q*q)/(rad*rad)
+            length = rad * 2 - 1
+            mid = length / 2
+            q = np.array(range(mid - 1, -1, -1)+range(-1, mid))
+            a = alpha * (rad * rad - q * q) / (rad * rad)
             a[mid] = 0
             self.a_s[(alpha, rad)] = a
             return a
     
     def alterneigh(self, alpha, rad, i, b, g, r):
-        if i-rad >= self.SPECIALS-1:
-            lo = i-rad
+        if i - rad >= self.SPECIALS - 1:
+            lo = i - rad
             start = 0
         else:
-            lo = self.SPECIALS-1
-            start = (self.SPECIALS-1 - (i-rad))
+            lo = self.SPECIALS - 1
+            start = (self.SPECIALS - 1 - (i - rad))
             
         if i+rad <= self.NETSIZE:
-            hi = i+rad
-            end = rad*2-1
+            hi = i + rad
+            end = rad * 2 - 1
         else:
             hi = self.NETSIZE
             end = (self.NETSIZE - (i+rad))
@@ -225,27 +226,6 @@ class NeuQuant:
         
         p = self.network[lo+1:hi]
         p -= np.transpose(np.transpose(p - np.array([b, g, r])) * a)
-    
-    # This was like this when I got here.
-    '''
-    def contest(self, b, g, r):
-        """ Search for biased BGR values
-                Finds closest neuron (min dist) and updates self.freq
-                finds best neuron (min dist-self.bias) and returns position
-                for frequently chosen neurons, self.freq[i] is high and self.bias[i] is negative
-                self.bias[i] = self.GAMMA*((1/self.NETSIZE)-self.freq[i])"""
-        
-        i, j = self.SPECIALS, self.NETSIZE
-        dists = abs(self.network[i:j] - np.array([b,g,r])).sum(1)
-        bestpos = i + np.argmin(dists)
-        biasdists = dists - self.bias[i:j]
-        bestbiaspos = i + np.argmin(biasdists)
-        self.freq[i:j] -= self.BETA * self.freq[i:j]
-        self.bias[i:j] += self.BETAGAMMA * self.freq[i:j]
-        self.freq[bestpos] += self.BETA
-        self.bias[bestpos] -= self.BETAGAMMA
-        return bestbiaspos
-    '''
     
     def contest(self, b, g, r):
         """ Search for biased BGR values
@@ -273,7 +253,7 @@ class NeuQuant:
     
     def learn(self):
         biasRadius = self.INITBIASRADIUS
-        alphadec = 30 + ((self.samplefac-1)/3)
+        alphadec = 30 + ((self.samplefac - 1) / 3)
         lengthcount = self.pixels.size
         samplepixels = lengthcount / self.samplefac
         delta = samplepixels / self.NCYCLES
@@ -284,26 +264,30 @@ class NeuQuant:
         if rad <= 1:
             rad = 0
         
-        print "Beginning 1D learning: samplepixels =",samplepixels," rad =", rad
+        logg.info("Beginning 1D learning: samplepixels = " + str(samplepixels) + ", rad = " + str(rad))
         
         step = 0
         pos = 0
-        if lengthcount%NeuQuant.PRIME1 != 0:
+        if lengthcount % NeuQuant.PRIME1 != 0:
             step = NeuQuant.PRIME1
-        elif lengthcount%NeuQuant.PRIME2 != 0:
+        elif lengthcount % NeuQuant.PRIME2 != 0:
             step = NeuQuant.PRIME2
-        elif lengthcount%NeuQuant.PRIME3 != 0:
+        elif lengthcount % NeuQuant.PRIME3 != 0:
             step = NeuQuant.PRIME3
         else:
             step = NeuQuant.PRIME4
         
         i = 0
         printed_string = ''
+        
         while i < samplepixels:
-            if i%100 == 99:
-                tmp = '\b'*len(printed_string)
-                printed_string = str((i+1)*100/samplepixels)+"%\n"
-                print tmp + printed_string,
+            
+            if i % 100 == 99:
+                tmp = '\b' * len(printed_string)
+                printed_string = str((i + 1) * 100 / samplepixels) + "%\n"
+                
+                #print tmp + printed_string,
+            
             p = self.pixels[pos]
             r = (p >> 16) & 0xff
             g = (p >>  8) & 0xff
@@ -322,16 +306,17 @@ class NeuQuant:
                 if rad > 0:
                     self.alterneigh(a, rad, j, b, g, r)
             
-            pos = (pos+step)%lengthcount
+            pos = (pos + step) % lengthcount
             
             i += 1
-            if i%delta == 0:
+            if i % delta == 0:
                 alpha -= alpha / alphadec
                 biasRadius -= biasRadius / self.RADIUSDEC
                 rad = biasRadius >> self.RADIUSBIASSHIFT
                 if rad <= 1:
                     rad = 0
-        print "Finished 1D learning: final alpha =",(1.0*alpha)/self.INITALPHA,"!"
+        
+        logg.info("Finished 1D learning: final alpha = %s !" % ((1.0 * alpha) / self.INITALPHA))
     
     def fix(self):
         for i in range(self.NETSIZE):
@@ -351,7 +336,7 @@ class NeuQuant:
             smallpos = i
             smallval = p[1] # Index on g
             # Find smallest in i..self.NETSIZE-1
-            for j in range(i+1, self.NETSIZE):
+            for j in range(i + 1, self.NETSIZE):
                 q = self.colormap[j]
                 if q[1] < smallval: # Index on g
                     smallpos = j
@@ -360,17 +345,17 @@ class NeuQuant:
             q = self.colormap[smallpos]
             # Swap p (i) and q (smallpos) entries
             if i != smallpos:
-                p[:],q[:] = q, p.copy()
+                p[:], q[:] = q, p.copy()
             
             # smallval entry is now in position i
             if smallval != previouscol:
-                self.netindex[previouscol] = (startpos+i) >> 1
-                for j in range(previouscol+1, smallval):
+                self.netindex[previouscol] = (startpos + i) >> 1
+                for j in range(previouscol + 1, smallval):
                     self.netindex[j] = i
                 previouscol = smallval
                 startpos = i
         self.netindex[previouscol] = (startpos+self.MAXNETPOS) >> 1
-        for j in range(previouscol+1, 256): # Really 256
+        for j in range(previouscol + 1, 256): # Really 256
             self.netindex[j] = self.MAXNETPOS
     
     def paletteImage(self):
@@ -382,7 +367,7 @@ class NeuQuant:
             for i in range(self.NETSIZE):
                 palette.extend(self.colormap[i][:3])
                 
-            palette.extend([0]*(256-self.NETSIZE)*3)
+            palette.extend([0] * (256-self.NETSIZE) * 3)
             
             # a palette image to use for quant
             self.pimage = Image.new("P", (1, 1), 0)
@@ -394,20 +379,21 @@ class NeuQuant:
         if cKDTree:
             return self.quantize_with_scipy(image)
         else:
-            print 'Scipy not available, falling back to slower version.'
+            logg.warning('Scipy not available, falling back to slower version.')
             return self.quantize_without_scipy(image)
     
     def quantize_with_scipy(self, image):
-        w,h = image.size
+        w, h = image.size
         px = np.asarray(image).copy()
-        px2 = px[:,:,:3].reshape((w*h,3))
+        px2 = px[:,:,:3].reshape((w * h,3))
         
-        kdtree = cKDTree(self.colormap[:,:3],leafsize=10)
+        kdtree = cKDTree(self.colormap[:,:3], leafsize=10)
         result = kdtree.query(px2)
         colorindex = result[1]
-        print "Distance:", (result[0].sum()/(w*h))
-        px2[:] = self.colormap[colorindex,:3]
         
+        logg.info("Distance: %s" % (result[0].sum() / (w * h)))
+        
+        px2[:] = self.colormap[colorindex,:3]
         return Image.fromarray(px).convert("RGB").quantize(palette=self.paletteImage())
     
     def quantize_without_scipy(self, image):
@@ -419,13 +405,13 @@ class NeuQuant:
         memo = {}
         for j in range(w):
             for i in range(h):
-                key = (px[i,j,0],px[i,j,1],px[i,j,2])
+                key = (px[i,j,0], px[i,j,1], px[i,j,2])
                 try:
                     val = memo[key]
                 except KeyError:
                     val = self.convert(key)
                     memo[key] = val
-                px[i,j,0],px[i,j,1],px[i,j,2] = val
+                px[i,j,0], px[i,j,1], px[i,j,2] = val
         return Image.fromarray(px).convert("RGB").quantize(palette=self.paletteImage())
     
     def convert(self, (r, g, b)):
@@ -435,6 +421,49 @@ class NeuQuant:
     def inxsearch(self, r, g, b):
         """Search for BGR values 0..255 and return colour index"""
         dists = (self.colormap[:,:3] - np.array([r,g,b]))
-        a= np.argmin((dists*dists).sum(1))
+        a = np.argmin((dists * dists).sum(1))
         return a
+
+if __name__ == "__main__":
+    from django.core.management import setup_environ
+    import settings
+    setup_environ(settings)
+    from jogging import logging as logg
+    import urllib2, StringIO, random
+    
+    urls = [
+        'http://ost2.s3.amazonaws.com/images/_uploads/IfThen_Detail_Computron_Orange_2to3_010.jpg',
+        'http://ost2.s3.amazonaws.com/images/_uploads/2805163544_1321ee6d30_o.jpg',
+        'http://ost2.s3.amazonaws.com/images/_uploads/IfThen_Detail_Computron_Silver_2to3_010.jpg',
+        'http://ost2.s3.amazonaws.com/images/_uploads/Josef_Muller_Brockmann_Detail_Lights_2to3_000.jpg',
+        'http://ost2.s3.amazonaws.com/images/_uploads/P4141870.jpg',
+        'http://ost2.s3.amazonaws.com/images/_uploads/After_The_Quake_Detail_Text_2to3_000.jpg',
+        'http://ost2.s3.amazonaws.com/images/_uploads/P4141477.jpg',
+        'http://ost2.s3.amazonaws.com/images/_uploads/P4141469.jpg',
+        'http://ost2.s3.amazonaws.com/images/_uploads/IMG_1310.jpg',
+        'http://ost2.s3.amazonaws.com/images/_uploads/P4141472.jpg',
+    ]
+    
+    random.seed()
+    imgurl = random.choice(urls)
+    
+    logg.info("Loading image: %s" % imgurl)
+    imgstr = urllib2.urlopen(imgurl).read()
+    img = Image.open(StringIO.StringIO(imgstr))
+    
+    logg.info("NeuQuantizing with samplefac=10...")
+    quant = NeuQuant(img.convert("RGBA"), 10)
+    
+    if np:
+        out = np.array(quant.colormap).reshape(16, 16, 4)
+        #out.T[0:3].T
+        outimg = Image.new('RGBA', (256, 1), (0,0,0))
+        outimg.putdata([tuple(t[0]) for t in out.T[0:3].T.reshape(256, 1, 3).tolist()])
+        a = outimg.resize((512, 256), Image.NEAREST)
+        a.show()
+    
+    else:
+        logg.info("**** QUANT! %s" % quant)
+    
+
 
