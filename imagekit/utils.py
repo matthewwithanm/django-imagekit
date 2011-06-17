6b1aff64c2986fccd134888bb96cf29ec070d5f4
Shanto <shanto@hotmail.com>
2011-06-17 13:13:20 -0700

    Fixes: CMYK files from Photoshop fails to load with exception -2

--- imagekit/utils.py
+++ imagekit/utils.py
@@ -4,6 +4,6 @@ import tempfile
 
 def img_to_fobj(img, format, **kwargs):
     tmp = tempfile.TemporaryFile()
-    img.save(tmp, format, **kwargs)
+    img.convert('RGB').save(tmp, format, **kwargs)
     tmp.seek(0)
     return tmp
