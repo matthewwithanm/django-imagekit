df93146049fcacebca4c68ad51ab5b4c091eac6a
Bryan Veloso <bryan@revyver.com>
2011-09-12 13:03:28 -0700

    Making versioning a little easier on myself.

--- imagekit/__init__.py
+++ imagekit/__init__.py
@@ -1,11 +1,13 @@
 """
 Django ImageKit
 
-Author: Justin Driscoll <justin.driscoll@gmail.com>
-Version: 0.4.0
+:author: Justin Driscoll <justin.driscoll@gmail.com>
+:maintainer: Bryan Veloso <bryan@revyver.com>
+:license: BSD
 
 """
+__title__ = 'django-imagekit'
+__version__ = '0.4.1'
+__build__ = 0x000400
 __author__ = 'Justin Driscoll, Bryan Veloso, Greg Newman, Chris Drackett'
-__version__ = (0, 4, 0)
-
-
+__license__ = 'BSD'
