6412e40c695c4239c0f77fee45e97434330d9b56
Chris McKenzie <Chrismc@hzdg.com>
2011-09-26 16:38:55 -0400

    adding test for new api

--- imagekit/tests.py
+++ imagekit/tests.py
@@ -1,49 +1,21 @@
 import os
 import tempfile
-import unittest
-from django.conf import settings
+import Image
 from django.core.files.base import ContentFile
 from django.db import models
 from django.test import TestCase
-
-from imagekit import processors
-from imagekit.models import ImageModel
-from imagekit.models import ImageSpec
-from imagekit.lib import Image
+from imagekit.models import ImageSpec, AdminThumbnailView
+from imagekit.processors.resize import Crop
+from imagekit.processors import Adjust
 
 
-class ResizeToWidth(processors.Resize):
-    width = 100
 
-class ResizeToHeight(processors.Resize):
-    height = 100
-
-class ResizeToFit(processors.Resize):
-    width = 100
-    height = 100
-
-class ResizeCropped(ResizeToFit):
-    crop = ('center', 'center')
-
-class TestResizeToWidth(ImageSpec):
-    access_as = 'to_width'
-    processors = [ResizeToWidth]
-
-class TestResizeToHeight(ImageSpec):
-    access_as = 'to_height'
-    processors = [ResizeToHeight]
-
-class TestResizeCropped(ImageSpec):
-    access_as = 'cropped'
-    processors = [ResizeCropped]
-
-class TestPhoto(ImageModel):
-    """ Minimal ImageModel class for testing """
-    image = models.ImageField(upload_to='images')
-
-    class IKOptions:
-        spec_module = 'imagekit.tests'
-
+class Photo(models.Model):
+    original_image = models.ImageField(upload_to='photos')
+    thumbnail = ImageSpec([Adjust(contrast=1.2, sharpness=1.1), Crop(50, 50)],
+            image_field='original_image', format='JPEG', quality=90)
+    admin_thumbnail_view = AdminThumbnailView(image_field='thumbnail')
+    
 
 class IKTest(TestCase):
     """ Base TestCase class """
@@ -52,48 +24,32 @@ class IKTest(TestCase):
         Image.new('RGB', (800, 600)).save(tmp, 'JPEG')
         tmp.seek(0)
         return tmp
-
+    
     def setUp(self):
-        self.p = TestPhoto()
+        self.photo = Photo()
         img = self.generate_image()
-        self.p.save_image('test.jpeg', ContentFile(img.read()))
-        self.p.save()
+        file = ContentFile(img.read())
+        self.photo.original_image = file
+        self.photo.original_image.save('test.jpeg', file)
+        self.photo.save()
         img.close()
-
+    
     def test_save_image(self):
-        img = self.generate_image()
-        path = self.p.image.path
-        self.p.save_image('test2.jpeg', ContentFile(img.read()))
-        self.failIf(os.path.isfile(path))
-        path = self.p.image.path
-        img.seek(0)
-        self.p.save_image('test.jpeg', ContentFile(img.read()))
-        self.failIf(os.path.isfile(path))
-        img.close()
-
+        photo = Photo.objects.get(id=self.photo.id)
+        self.assertTrue(os.path.isfile(photo.original_image.path))
+        
     def test_setup(self):
-        self.assertEqual(self.p.image.width, 800)
-        self.assertEqual(self.p.image.height, 600)
+        self.assertEqual(self.photo.original_image.width, 800)
+        self.assertEqual(self.photo.original_image.height, 600)
+    
+    def test_thumbnail_creation(self):
+        photo = Photo.objects.get(id=self.photo.id)
+        self.assertTrue(os.path.isfile(photo.thumbnail.file.name))
 
-    def test_to_width(self):
-        self.assertEqual(self.p.to_width.width, 100)
-        self.assertEqual(self.p.to_width.height, 75)
-
-    def test_to_height(self):
-        self.assertEqual(self.p.to_height.width, 133)
-        self.assertEqual(self.p.to_height.height, 100)
-
-    def test_crop(self):
-        self.assertEqual(self.p.cropped.width, 100)
-        self.assertEqual(self.p.cropped.height, 100)
-
-    def test_url(self):
-        tup = (settings.MEDIA_URL, self.p._ik.cache_dir,
-               'images/test_to_width.jpeg')
-        self.assertEqual(self.p.to_width.url, "%s%s/%s" % tup)
-
-    def tearDown(self):
-        # make sure image file is deleted
-        path = self.p.image.path
-        self.p.delete()
-        self.failIf(os.path.isfile(path))
+    def test_thumbnail_size(self):
+        self.assertEqual(self.photo.thumbnail.width, 50)
+        self.assertEqual(self.photo.thumbnail.height, 50)
+    
+    def test_thumbnail_source_file(self):
+        self.assertEqual(
+            self.photo.thumbnail.source_file, self.photo.original_image)
