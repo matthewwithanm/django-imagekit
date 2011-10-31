28f4dd01032a1ff474abce3b79398e66bbaa9249
Bryan Veloso <bryan@revyver.com>
2011-11-01 00:44:29 +0900

    Develop is now v1.1.0.dev.

--- imagekit/__init__.py
+++ imagekit/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'django-imagekit'
 __author__ = 'Justin Driscoll, Bryan Veloso, Greg Newman, Chris Drackett, Matthew Tretter, Eric Eldredge'
-__version__ = (1, 0, 0, 'final', 0)
+__version__ = (1, 1, 0, 'dev', 0)
 __build__ = 0x001000
 __license__ = 'BSD'
