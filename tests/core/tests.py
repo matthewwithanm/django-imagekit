7ce43309ad3346d95fe138175ee2ebe65c3764b8
FI$H 2000 <fish2000@gmail.com>
2011-11-09 16:23:04 -0500

    Adds SmartCrop resize processor, with tests.

--- tests/core/tests.py
+++ tests/core/tests.py
@@ -9,12 +9,16 @@ from imagekit import utils
 from imagekit.lib import Image
 from imagekit.models import ImageSpec
 from imagekit.processors import Adjust
-from imagekit.processors.resize import Crop
+from imagekit.processors.resize import Crop, SmartCrop
 
 
 class Photo(models.Model):
     original_image = models.ImageField(upload_to='photos')
+    
     thumbnail = ImageSpec([Adjust(contrast=1.2, sharpness=1.1), Crop(50, 50)],
+            image_field='original_image', format='JPEG', quality=90)
+    
+    smartcropped_thumbnail = ImageSpec([Adjust(contrast=1.2, sharpness=1.1), SmartCrop(50, 50)],
             image_field='original_image', format='JPEG', quality=90)
 
 
@@ -24,32 +28,50 @@ class IKTest(TestCase):
         Image.new('RGB', (800, 600)).save(tmp, 'JPEG')
         tmp.seek(0)
         return tmp
-
+    
+    def generate_lenna(self):
+        """
+        See also:
+        
+        http://en.wikipedia.org/wiki/Lenna
+        http://sipi.usc.edu/database/database.php?volume=misc&image=12
+        
+        """
+        tmp = tempfile.TemporaryFile()
+        lennapath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets', 'lenna-800x600-white-border.jpg')
+        with open(lennapath, "r+b") as lennafile:
+            Image.open(lennafile).save(tmp, 'JPEG')
+        tmp.seek(0)
+        return tmp
+    
     def setUp(self):
         self.photo = Photo()
-        img = self.generate_image()
+        img = self.generate_lenna()
         file = ContentFile(img.read())
         self.photo.original_image = file
         self.photo.original_image.save('test.jpeg', file)
         self.photo.save()
         img.close()
-
+    
     def test_save_image(self):
         photo = Photo.objects.get(id=self.photo.id)
         self.assertTrue(os.path.isfile(photo.original_image.path))
-
+    
     def test_setup(self):
         self.assertEqual(self.photo.original_image.width, 800)
         self.assertEqual(self.photo.original_image.height, 600)
-
+    
     def test_thumbnail_creation(self):
         photo = Photo.objects.get(id=self.photo.id)
         self.assertTrue(os.path.isfile(photo.thumbnail.file.name))
-
+    
     def test_thumbnail_size(self):
+        """ Explicit and smart-cropped thumbnail size """
         self.assertEqual(self.photo.thumbnail.width, 50)
         self.assertEqual(self.photo.thumbnail.height, 50)
-
+        self.assertEqual(self.photo.smartcropped_thumbnail.width, 50)
+        self.assertEqual(self.photo.smartcropped_thumbnail.height, 50)
+    
     def test_thumbnail_source_file(self):
         self.assertEqual(
             self.photo.thumbnail.source_file, self.photo.original_image)
