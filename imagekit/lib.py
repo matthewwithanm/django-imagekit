cfd503a2eb801bca868d0fee56ccd2b529dab24c
Matthew Tretter <matthew@exanimo.com>
2011-11-15 23:42:43 -0500

    TrimColor processor

--- imagekit/lib.py
+++ imagekit/lib.py
@@ -1,7 +1,8 @@
 # Required PIL classes may or may not be available from the root namespace
 # depending on the installation method used.
 try:
-    from PIL import Image, ImageColor, ImageChops, ImageEnhance, ImageFile, ImageFilter
+    from PIL import Image, ImageColor, ImageChops, ImageEnhance, ImageFile, \
+            ImageFilter, ImageDraw, ImageStat
 except ImportError:
     try:
         import Image
@@ -10,5 +11,7 @@ except ImportError:
         import ImageEnhance
         import ImageFile
         import ImageFilter
+        import ImageDraw
+        import ImageStat
     except ImportError:
         raise ImportError('ImageKit was unable to import the Python Imaging Library. Please confirm it`s installed and available on your current Python path.')
