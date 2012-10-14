7ce05f468a3b6a7f0d9f325ec3a6b952f1da04a5
Matthew Tretter <m@tthewwithanm.com>
2012-10-14 15:41:35 -0400

    Pass attname to dispatch_signal
    This allows any file object to be used--even those that don't have a
    `field` attribute. For example, you could theoretically use one spec
    field as a source for another.

--- imagekit/specs/sources.py
+++ imagekit/specs/sources.py
@@ -65,20 +65,20 @@ class ModelSignalRouter(object):
             new_hashes = self.update_source_hashes(instance)
             for attname, file in self.get_field_dict(instance).items():
                 if created:
-                    self.dispatch_signal(source_created, file, sender)
+                    self.dispatch_signal(source_created, file, sender, attname)
                 elif old_hashes[attname] != new_hashes[attname]:
-                    self.dispatch_signal(source_changed, file, sender)
+                    self.dispatch_signal(source_changed, file, sender, attname)
 
     @ik_model_receiver
     def post_delete_receiver(self, sender, instance=None, **kwargs):
         for attname, file in self.get_field_dict(instance):
-            self.dispatch_signal(source_deleted, file, sender)
+            self.dispatch_signal(source_deleted, file, sender, attname)
 
     @ik_model_receiver
     def post_init_receiver(self, sender, instance=None, **kwargs):
         self.update_source_hashes(instance)
 
-    def dispatch_signal(self, signal, file, model_class):
+    def dispatch_signal(self, signal, file, model_class, attname):
         """
         Dispatch the signal for each of the matching sources. Note that more
         than one source can have the same model and image_field; it's important
@@ -86,8 +86,7 @@ class ModelSignalRouter(object):
 
         """
         for source in self._sources:
-            # TODO: Is it okay to require a field attribute on our file?
-            if source.model_class is model_class and source.image_field == file.field.attname:
+            if source.model_class is model_class and source.image_field == attname:
                 signal.send(sender=source, source_file=file)
 
 
