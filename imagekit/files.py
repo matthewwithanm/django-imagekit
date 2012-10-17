806ebd75b63ae20e526d9fb0f44d9e05dd688768
Matthew Tretter <m@tthewwithanm.com>
2012-10-17 00:37:02 -0400

    Don't return file from generate()
    The file is `self`

--- imagekit/files.py
+++ imagekit/files.py
@@ -111,7 +111,7 @@ class ImageSpecCacheFile(BaseIKFile, ImageFile):
         if self.source_file:  # TODO: Should we error here or something if the source_file doesn't exist?
             # Process the original image file.
             content = self.spec.apply(self.source_file)
-            return self.storage.save(self.name, content)
+            self.storage.save(self.name, content)
 
 
 class IKContentFile(ContentFile):
