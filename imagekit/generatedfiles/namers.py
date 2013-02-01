58e1c7f7e0ceaebd7d2515380271dcf2c2428fa8
Matthew Tretter <m@tthewwithanm.com>
2013-02-01 01:27:54 -0500

    Some docs

--- imagekit/generatedfiles/namers.py
+++ imagekit/generatedfiles/namers.py
@@ -1,9 +1,29 @@
+"""
+Functions responsible for returning filenames for the given image generator.
+Users are free to define their own functions; these are just some some sensible
+choices.
+
+"""
+
 from django.conf import settings
 import os
 from ..utils import format_to_extension, suggest_extension
 
 
 def source_name_as_path(generator):
+    """
+    A namer that, given the following source file name::
+
+        photos/thumbnails/bulldog.jpg
+
+    will generate a name like this::
+
+        /path/to/generated/images/photos/thumbnails/bulldog/5ff3233527c5ac3e4b596343b440ff67.jpg
+
+    where "/path/to/generated/images/" is the value specified by the
+    ``IMAGEKIT_GENERATEDFILE_DIR`` setting.
+
+    """
     source_filename = getattr(generator.source, 'name', None)
 
     if source_filename is None or os.path.isabs(source_filename):
@@ -21,6 +41,19 @@ def source_name_as_path(generator):
 
 
 def source_name_dot_hash(generator):
+    """
+    A namer that, given the following source file name::
+
+        photos/thumbnails/bulldog.jpg
+
+    will generate a name like this::
+
+        /path/to/generated/images/photos/thumbnails/bulldog.5ff3233527c5.jpg
+
+    where "/path/to/generated/images/" is the value specified by the
+    ``IMAGEKIT_GENERATEDFILE_DIR`` setting.
+
+    """
     source_filename = getattr(generator.source, 'name', None)
 
     if source_filename is None or os.path.isabs(source_filename):
@@ -39,6 +72,19 @@ def source_name_dot_hash(generator):
 
 
 def hash(generator):
+    """
+    A namer that, given the following source file name::
+
+        photos/thumbnails/bulldog.jpg
+
+    will generate a name like this::
+
+        /path/to/generated/images/5ff3233527c5ac3e4b596343b440ff67.jpg
+
+    where "/path/to/generated/images/" is the value specified by the
+    ``IMAGEKIT_GENERATEDFILE_DIR`` setting.
+
+    """
     format = getattr(generator, 'format', None)
     ext = format_to_extension(format) if format else ''
     return os.path.normpath(os.path.join(settings.IMAGEKIT_GENERATEDFILE_DIR,
