28ffd444d6d7bbb68b39dc4e318b56f4dd9c8b15
Matthew Tretter <m@tthewwithanm.com>
2013-04-02 23:01:38 -0400

    Relax Pillow testing requirement

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'nose==1.2.1',
         'nose-progressive==1.3',
         'django-nose==1.1',
-        'Pillow==1.7.8',
+        'Pillow<3.0',
     ],
     test_suite='testrunner.run_tests',
     install_requires=[
