c6a0a13c45656ff98984b85a34da91242f32b41c
Matthew Tretter <m@tthewwithanm.com>
2013-05-25 00:02:26 -0400

    Test that source_saved is dispatched for new instance

--- tests/test_sourcegroups.py
+++ tests/test_sourcegroups.py
@@ -2,7 +2,7 @@ from django.core.files import File
 from imagekit.signals import source_saved
 from imagekit.specs.sourcegroups import ImageFieldSourceGroup
 from nose.tools import eq_
-from . models import AbstractImageModel, ConcreteImageModel
+from . models import AbstractImageModel, ImageModel, ConcreteImageModel
 from .utils import get_image_file
 
 
@@ -12,6 +12,19 @@ def make_counting_receiver(source_group):
             receiver.count += 1
     receiver.count = 0
     return receiver
+
+
+def test_source_saved_signal():
+    """
+    Creating a new instance with an image causes the source_saved signal to be
+    dispatched.
+
+    """
+    source_group = ImageFieldSourceGroup(ImageModel, 'image')
+    receiver = make_counting_receiver(source_group)
+    source_saved.connect(receiver)
+    ImageModel.objects.create(image=File(get_image_file()))
+    eq_(receiver.count, 1)
 
 
 def test_abstract_model_signals():
