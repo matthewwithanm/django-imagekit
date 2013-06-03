b1593ad56d8793469e05c8c29d4530fd74f087d2
Jacob Magnusson <m@jacobian.se>
2013-06-03 15:16:54 +0200

    Fix: CACHES['default'] isn't a string generateimages would throw "TypeError: unhashable type 'dict'" if the new CACHES setting was used. This was because the code assumed that CACHES.get('default') was a string, when it should be a dict.

--- imagekit/conf.py
+++ imagekit/conf.py
@@ -25,14 +25,21 @@ class ImageKitConf(AppConf):
             else:
                 dummy_cache = '{0.__module__}.{0.__name__}'.format(DummyCache)
 
+            # DEFAULT_CACHE_ALIAS doesn't exist in Django<=1.2
+            try:
+                from django.core.cache import DEFAULT_CACHE_ALIAS
+                default_cache_alias = DEFAULT_CACHE_ALIAS
+            except ImportError:
+                default_cache_alias = 'default'
+
             if settings.DEBUG:
                 value = dummy_cache
             else:
-                value = (
-                    getattr(settings, 'CACHES', {}).get('default')
-                    or getattr(settings, 'CACHE_BACKEND', None)
-                    or dummy_cache
-                )
+                newstyle_cache = getattr(settings, 'CACHES', {}) \
+                                   .get(default_cache_alias, {}).get('BACKEND')
+                oldstyle_cache = getattr(settings, 'CACHE_BACKEND', None)
+                value = newstyle_cache or oldstyle_cache or dummy_cache
+
         return value
 
     def configure_default_file_storage(self, value):
