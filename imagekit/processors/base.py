cb883a4afef861fbdc734969a0bfe8f57ee02f92
Steven Wood <steven@ikoro.com>
2014-10-07 15:23:02 -0400

    Refer to pilkit (not imagekit) in deprecation warning

--- imagekit/processors/base.py
+++ imagekit/processors/base.py
@@ -2,6 +2,6 @@ import warnings
 
 from pilkit.processors.base import *
 
-warnings.warn('imagekit.processors.base is deprecated use imagekit.processors instead', DeprecationWarning)
+warnings.warn('imagekit.processors.base is deprecated use pilkit.processors instead', DeprecationWarning)
 
 __all__ = ['ProcessorPipeline', 'Adjust', 'Reflection', 'Transpose', 'Anchor', 'MakeOpaque']
