c919adce6f5edb9f084272d162d42834aefc7928
Michael Fladischer <FladischerMichael@fladi.at>
2016-02-09 10:40:06 +0100

    Update test environment to to use components that are already EOL.
    The release of Django 1.9 brought Django 1.7 to its end of life. Thus I dropped
    all the prior versions from the test suite.
    
    Python 2.6 has already gone EOL and 3.2 and 3.3 have started to only receive
    security updates so they also got dropped. My main motivation here is that those
    versions of the interpreter are also long gone from the Debian (sid) archive.
    
    I also removed the version caps on the test dependencies as packages like Pillow
    and mock have had major releases that were never used in the tests but could
    have already been deployed out there.

--- setup.py
+++ setup.py
@@ -43,12 +43,12 @@ setup(
     zip_safe=False,
     include_package_data=True,
     tests_require=[
-        'beautifulsoup4==4.4.0',
-        'nose>=1.3.6,<1.5',
-        'nose-progressive==1.5.1',
-        'django-nose>=1.2,<1.5',
-        'Pillow<3.0',
-        'mock==1.0.1',
+        'beautifulsoup4>=4.4.0',
+        'nose>=1.3.6',
+        'nose-progressive>=1.5.1',
+        'django-nose>=1.4',
+        'Pillow>=3',
+        'mock>=1.0.1',
     ],
     test_suite='testrunner.run_tests',
     install_requires=[
@@ -68,12 +68,10 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Utilities'
     ],
 )
