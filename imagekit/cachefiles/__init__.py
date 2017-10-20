00fcb6f6d0e3ad6f298d7e5dd42a48ded998ac06
Roman Gorbil <gorbil@saritasa.com>
2017-10-20 18:26:37 +0700

    Fix pickle serialization for ImageCacheFile
    When Celery CachedFileBackend used with filesystem storage (django.core.files.storage.FileSystemStorage), everything works fine.
    But there are issues with storages.backends.s3boto3.S3Boto3Storage (and it's fix from #391), as well as with django_s3_storage.storage.S3Storage.
    
    Exception was:
    
    ```
    Traceback (most recent call last):
      ...
    
      File "/src/django-imagekit/imagekit/cachefiles/__init__.py", line 131, in __bool__
        existence_required.send(sender=self, file=self)
      ...
      File "/libs/utils.py", line 380, in on_existence_required
        file.generate()
      File "/src/django-imagekit/imagekit/cachefiles/__init__.py", line 94, in generate
        self.cachefile_backend.generate(self, force)
      File "/src/django-imagekit/imagekit/cachefiles/backends.py", line 136, in generate
        self.schedule_generation(file, force=force)
      File "/src/django-imagekit/imagekit/cachefiles/backends.py", line 165, in schedule_generation
        _celery_task.delay(self, file.generator, force=force)
      ...
      File "/lib/python3.6/site-packages/kombu/serialization.py", line 221, in dumps
        payload = encoder(data)
      File "/lib/python3.6/site-packages/kombu/serialization.py", line 350, in pickle_dumps
        return dumper(obj, protocol=pickle_protocol)
    kombu.exceptions.EncodeError: can't pickle _thread._local objects
    ```

--- imagekit/cachefiles/__init__.py
+++ imagekit/cachefiles/__init__.py
@@ -144,7 +144,23 @@ class ImageCacheFile(BaseIKFile, ImageFile):
         # file is hidden link to "file" attribute
         state.pop('_file', None)
 
+        # remove storage from state as some non-FileSystemStorage can't be
+        # pickled
+        settings_storage = get_singleton(
+            settings.IMAGEKIT_DEFAULT_FILE_STORAGE,
+            'file storage backend'
+        )
+        if state['storage'] == settings_storage:
+            state.pop('storage')
         return state
+
+    def __setstate__(self, state):
+        if 'storage' not in state:
+            state['storage'] = get_singleton(
+                settings.IMAGEKIT_DEFAULT_FILE_STORAGE,
+                'file storage backend'
+            )
+        self.__dict__.update(state)
 
     def __nonzero__(self):
         # Python 2 compatibility
