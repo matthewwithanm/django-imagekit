e49e1a7aab381b5aba2746281fac740490a09eaf
Jochen Wersdoerfer <jochen@wersdoerfer.de>
2019-05-30 17:41:20 +0200

    when storing media files in s3 content.seek(0) raises 'ValueError: I/O operation on closed file.' - ignoring it just works.

--- imagekit/cachefiles/__init__.py
+++ imagekit/cachefiles/__init__.py
@@ -100,7 +100,11 @@ class ImageCacheFile(BaseIKFile, ImageFile):
         actual_name = self.storage.save(self.name, content)
 
         # We're going to reuse the generated file, so we need to reset the pointer.
-        content.seek(0)
+        try:
+            content.seek(0)
+        except ValueError as e:
+            # I get I/O operation on closed file. Ignoring it works.
+            pass
 
         # Store the generated file. If we don't do this, the next time the
         # "file" attribute is accessed, it will result in a call to the storage
