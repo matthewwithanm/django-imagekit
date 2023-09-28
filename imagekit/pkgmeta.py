07f2b3c964d1943043d432556551dd60a0a9ca41
Venelin Stoykov <vkstoykov@gmail.com>
2023-09-28 03:17:11 +0300

    Bump version to 5.0.0

--- imagekit/pkgmeta.py
+++ imagekit/pkgmeta.py
@@ -1,5 +1,5 @@
 __title__ = 'django-imagekit'
 __author__ = 'Matthew Tretter, Venelin Stoykov, Eric Eldredge, Bryan Veloso, Greg Newman, Chris Drackett, Justin Driscoll'
-__version__ = '4.1.0'
+__version__ = '5.0.0'
 __license__ = 'BSD'
 __all__ = ['__title__', '__author__', '__version__', '__license__']
