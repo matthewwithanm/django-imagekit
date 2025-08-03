d01e0f67e7bf890c2569ee013b73486da5975860
Bryan Veloso <bryan@velo.so>
2025-08-03 10:47:27 -0700

    execfile() is a Python 2 thing.

--- docs/conf.py
+++ docs/conf.py
@@ -47,8 +47,9 @@ project = 'ImageKit'
 copyright = '2011, Justin Driscoll, Bryan Veloso, Greg Newman, Chris Drackett & Matthew Tretter'
 
 pkgmeta = {}
-execfile(os.path.join(os.path.dirname(__file__), '..', 'imagekit',
-                      'pkgmeta.py'), pkgmeta)
+pkgmeta_file = os.path.join(os.path.dirname(__file__), '..', 'imagekit', 'pkgmeta.py')
+with open(pkgmeta_file, 'r') as f:
+    exec(f.read(), pkgmeta)
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
